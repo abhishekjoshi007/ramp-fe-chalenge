75487ed79f3efefa2a2f91915ae8f67ea59c737d
Abhishek Joshi <32777573+abhishekjoshi007@users.noreply.github.com>
2024-07-10 01:33:22 -0500

    Bug 6  - View more Getting Diappear

--- src/App.tsx
+++ src/App.tsx
@@ -73,18 +73,17 @@ export function App() {
                 <div className="RampGrid">
                     <Transactions transactions={transactions} />
 
-                    {transactions !== null && (
-                        <button
-                            className="RampButton"
-                            disabled={paginatedTransactionsUtils.loading || !paginatedTransactions?.nextPage}
-                            onClick={async () => {
-                                if (paginatedTransactions?.nextPage)
-                                    await loadAllTransactions()
-                            }}
-                        >
-                            View More
-                        </button>
-                    )}
+                    {transactions !== null && !!paginatedTransactions?.nextPage && (
+                      <button
+                        className="RampButton"
+                        disabled={paginatedTransactionsUtils.loading}
+                        onClick={async () => {
+                          await loadAllTransactions()
+                        }}
+                      >
+                        View More
+                      </button>
+          )}
                 </div>
             </main>
         </Fragment>
